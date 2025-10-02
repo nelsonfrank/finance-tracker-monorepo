c4b068d6ba3ec3f4ed673a9e9dcdb5244929ee62
nelsonfrank <nelsonfrank741@gmail.com>
2025-10-02 22:41:28 +0300

    fix(api): Add errors utils fns

---
+++ apps/api/cmd/api/json.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func writeJSON(w http.ResponseWriter, status int, data any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(data)
+}
+
+func writeJSONError(w http.ResponseWriter, status int, message string) error {
+	type envelope struct {
+		Error string `json:"error"`
+	}
+
+	return writeJSON(w, status, &envelope{Error: message})
+}
--- apps/api/cmd/api/middleware.go
+++ apps/api/cmd/api/middleware.go
@@ -19,17 +19,20 @@ func (app *application) AuthTokenMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authHeader := r.Header.Get("Authorization")
 		if authHeader == "" {
+			app.unauthorizedErrorResponse(w, r, fmt.Errorf("authorization header is missing"))
 			return
 		}
 
 		parts := strings.Split(authHeader, " ")
 		if len(parts) != 2 || parts[0] != "Bearer" {
+			app.unauthorizedErrorResponse(w, r, fmt.Errorf("authorization header is malformed"))
 			return
 		}
 
 		token := parts[1]
 		jwtToken, err := app.authenticator.ValidateToken(token)
 		if err != nil {
+			app.unauthorizedErrorResponse(w, r, err)
 			return
 		}
 
@@ -37,6 +40,7 @@ func (app *application) AuthTokenMiddleware(next http.Handler) http.Handler {
 
 		userID, err := strconv.ParseInt(fmt.Sprintf("%.f", claims["sub"]), 10, 64)
 		if err != nil {
+			app.unauthorizedErrorResponse(w, r, err)
 			return
 		}
 
@@ -44,6 +48,7 @@ func (app *application) AuthTokenMiddleware(next http.Handler) http.Handler {
 
 		user, err := app.getUser(ctx, userID)
 		if err != nil {
+			app.unauthorizedErrorResponse(w, r, err)
 			return
 		}
 
@@ -76,6 +81,7 @@ func (app *application) RefreshTokenMiddleware(next http.Handler) http.Handler {
 
 		jwtToken, err := app.authenticator.ValidateToken(cookie.Value)
 		if err != nil {
+			app.unauthorizedErrorResponse(w, r, err)
 			return
 		}
 
@@ -83,12 +89,14 @@ func (app *application) RefreshTokenMiddleware(next http.Handler) http.Handler {
 
 		userID, err := strconv.ParseInt(fmt.Sprintf("%.f", claims["sub"]), 10, 64)
 		if err != nil {
+			app.unauthorizedErrorResponse(w, r, err)
 			return
 		}
 
 		ctx := r.Context()
 		user, err := app.getUser(ctx, userID)
 		if err != nil {
+			app.unauthorizedErrorResponse(w, r, err)
 			return
 		}
 
